12524e0109f85753132a544f9b726c208a9a3983
mbeascoa <miguel.beascoa@beastek.net>
2021-10-20 19:16:44 +0200

    segundo commit

---
+++ Ejmplo1.py
@@ -0,0 +1,10 @@
+print("Hola Mundo")
+print("----------------------------------EJEMPLO SUMA NUMEROS ---------------------------------------")
+print("---- Introduce un primer número : ")
+numero1=5
+numero2=6
+resultado= numero1+numero2
+
+print("---- Introduce un segundo número : ")
+print("---- Sumamos los dos números y el resultado es " )
+print ("La suma es :" ,resultado)
